8a8c8a338fa34703c0158a7906099047813be450
Pietro Rocchio <pietro.rocchio@gmail.com>
2025-12-04 11:18:24 +0100

    feat(CCIR): add frequency and tone definitions for CCIR and PCCIR

--- src/protocolli/CCIR.py
+++ src/protocolli/CCIR.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # ==========================
-#  DEFINIZIONI CCIR
+#  CCIR FREQUENCY AND SYMBOL DEFINITIONS
 # ==========================
 CCIR_FREQS = {
     "1": 1124, "2": 1197, "3": 1275, "4": 1358, "5": 1446,
@@ -12,7 +12,7 @@ CCIR_SYMBOLS = list(CCIR_FREQS.keys())
 CCIR_VALUES = np.array(list(CCIR_FREQS.values()), dtype=float)
 
 # ==========================
-#  DEFINIZIONI PCCIR
+#  PCCIR FREQUENCY AND SYMBOL DEFINITIONS
 # ==========================
 PCCIR_FREQS = {
     "1": 1124, "2": 1197, "3": 1275, "4": 1358, "5": 1446,
@@ -22,9 +22,43 @@ PCCIR_FREQS = {
 PCCIR_SYMBOLS = list(PCCIR_FREQS.keys())
 PCCIR_VALUES = np.array(list(PCCIR_FREQS.values()), dtype=float)
 
+# ==========================
+#  CCIR CODE LENGTH DEFINITIONS
+# ==========================
 CCIR_CODE_LEN_MS = {
     "CCIR-1": 100,
     "CCIR-2": 70,
     "CCIR-7": 70,
     "PCCIR": 100
 }
+
+# ==========================
+#  CCIR REPEATER TONE DEFINITION
+# ==========================
+CCIR_TONE_CH_REPEATER = "E"
+CCIR_TONE_CH_REPEATER_FREQ = CCIR_FREQS[CCIR_TONE_CH_REPEATER]
+
+# ==========================
+#  CCIR GROUP TONE DEFINITION
+# ==========================
+CCIR_TONE_CH_GROUP = "A"
+CCIR_TONE_CH_GROUP_FREQ = CCIR_FREQS[CCIR_TONE_CH_GROUP]
+
+
+# ==========================
+#  PCCIR REPEATER TONE DEFINITION
+# ==========================
+PCCIR_TONE_CH_REPEATER = "E"
+PCCIR_TONE_CH_REPEATER_FREQ = PCCIR_FREQS[PCCIR_TONE_CH_REPEATER]
+
+# ==========================
+#  PCCIR GROUP TONE DEFINITION
+# ==========================
+PCCIR_TONE_CH_GROUP = "A"
+PCCIR_TONE_CH_GROUP_FREQ = PCCIR_FREQS[PCCIR_TONE_CH_GROUP]
+
+# ==========================
+#  PCCIR RESET TONE DEFINITION
+# ==========================
+CCIR_TONE_CH_RESET = "C"
+CCIR_TONE_CH_RESET_FREQ = CCIR_FREQS[CCIR_TONE_CH_RESET]
